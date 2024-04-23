3c3892a5c842011944740922b37c08c1c6e83f48
Cezar Augustus Lamann <cezar.lamann@gmail.com>
2024-04-23 22:40:57 +0200

    #6 - Added IDateTimeProvider and BaseDateTimeProvider

---
+++ src/Basionix.BaseEntities/Interfaces/IDateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Basionix.BaseEntities
+{
+    using System;
+
+    public interface IDateTimeProvider
+    {
+        DateTimeOffset UtcNowOffset { get; }
+        DateTime UtcNow { get; }
+    }
+}
